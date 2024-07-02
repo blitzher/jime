ceb3cb2ee23490b1da79c5dbf231e58e52aae309
blitzher <skovboorg@gmail.com>
2024-07-03 00:53:04 +0200

    File explorer cell renderer

--- src/JimeFileExplorer.java
+++ src/JimeFileExplorer.java
@@ -1,7 +1,12 @@
 import java.awt.Dimension;
+import java.awt.event.MouseListener;
+import java.io.File;
 import java.nio.file.Path;
 
+import javax.swing.JLabel;
 import javax.swing.JTree;
+import javax.swing.tree.TreeCellRenderer;
+
 import util.FileTreeModel;
 
 public class JimeFileExplorer extends JTree {
@@ -13,6 +18,48 @@ public class JimeFileExplorer extends JTree {
 
         root = _root;
         setModel(new FileTreeModel(root.toFile()));
+        setCellRenderer(new TreeCellRenderer() {
+            @Override
+            public java.awt.Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
+                    boolean expanded, boolean leaf, int row, boolean hasFocus) {
+
+                File f = (File) value;
+                JLabel label = new JLabel(f.getName());
+                label.setIcon(f.isDirectory() ? null : null);
+
+                label.addMouseListener(new java.awt.event.MouseAdapter() {
+                    public void mouseClicked(java.awt.event.MouseEvent e) {
+                        System.out.println("Clicked on " + f.getName());
+                    }
+                });
+
+                return label;
+            }
+
+        });
+
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                System.out.println("Clicked on " + getSelectionPath().getLastPathComponent());
+            }
+
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+            }
+        });
 
         setPreferredSize(
                 new Dimension(500, 600));
